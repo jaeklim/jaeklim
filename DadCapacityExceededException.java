7bd6f12c847585f506adf44c5667e73f146d541d
Jae Lim <ultracap@gmail.com>
2021-04-20 20:21:11 -0700

    Fix a missing serialVersionUID warning

--- DadCapacityExceededException.java
+++ DadCapacityExceededException.java
@@ -3,4 +3,5 @@
  * more than two.
  */
 public class DadCapacityExceededException extends Exception {
+	private static final long serialVersionUID = -8364347317862893603L;
 }
--- DeadlockException.java
+++ DeadlockException.java
@@ -2,4 +2,5 @@
  * An exception that can be thrown when a dad has to re-play with children.
  */
 public class DeadlockException extends Exception {
+	private static final long serialVersionUID = 5225385071459143614L;
 }
